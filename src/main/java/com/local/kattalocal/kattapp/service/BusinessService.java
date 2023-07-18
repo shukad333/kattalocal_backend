3304731a1578a8847dfcdd38880ff5803cc1fbbb
Shuhail Kadavath <skadavath@vmware.com>
2023-07-18 12:49:22 +0530

    deactivate business | list all business

--- src/main/java/com/local/kattalocal/kattapp/service/BusinessService.java
+++ src/main/java/com/local/kattalocal/kattapp/service/BusinessService.java
@@ -2,6 +2,8 @@ package com.local.kattalocal.kattapp.service;
 
 import com.local.kattalocal.kattapp.model.Business;
 import com.local.kattalocal.kattapp.repository.BusinessRepo;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,23 @@ public class BusinessService {
         business.setActive(true);
         return businessRepo.save(business);
 
+  }
+
+  public List<Business> getAllBusinesses() {
+      return businessRepo.findAll();
+  }
+
+  public void deactivateBusiness(Long businessId) {
+    updateBusinessStatus(businessId,false);
+  }
+
+  private void updateBusinessStatus(Long businessId,boolean status) {
+    Optional<Business> businessOptional = businessRepo.findById(businessId);
+    if(businessOptional.isPresent()) {
+      Business business = businessOptional.get();
+      business.setActive(status);
+      businessRepo.save(business);
+    }
   }
 
 }
