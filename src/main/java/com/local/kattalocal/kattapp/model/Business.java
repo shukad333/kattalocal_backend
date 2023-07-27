8c8073cbf5440ed4283e90076d72f03319d8a0be
Shuhail Kadavath <skadavath@vmware.com>
2023-07-27 14:14:52 +0530

    address attributes with business

--- src/main/java/com/local/kattalocal/kattapp/model/Business.java
+++ src/main/java/com/local/kattalocal/kattapp/model/Business.java
@@ -27,6 +27,34 @@ public class Business implements Serializable {
   private Double latitude;
   private Double longitude;
 
+  private String address1;
+  private String address2;
+  private String address3;
+
+  public String getAddress1() {
+    return address1;
+  }
+
+  public void setAddress1(String address1) {
+    this.address1 = address1;
+  }
+
+  public String getAddress2() {
+    return address2;
+  }
+
+  public void setAddress2(String address2) {
+    this.address2 = address2;
+  }
+
+  public String getAddress3() {
+    return address3;
+  }
+
+  public void setAddress3(String address3) {
+    this.address3 = address3;
+  }
+
   public String getLicenseNumber() {
     return licenseNumber;
   }
