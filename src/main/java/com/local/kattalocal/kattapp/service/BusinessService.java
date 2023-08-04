0c3377b6b3dc65e16287b88af4d657d925abc43d
Shuhail Kadavath <shukad333@gmail.com>
2023-08-04 20:19:46 +0530

    logging at service layer

--- src/main/java/com/local/kattalocal/kattapp/service/BusinessService.java
+++ src/main/java/com/local/kattalocal/kattapp/service/BusinessService.java
@@ -4,10 +4,12 @@ import com.local.kattalocal.kattapp.model.Business;
 import com.local.kattalocal.kattapp.repository.BusinessRepo;
 import java.util.List;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class BusinessService {
 
@@ -17,22 +19,25 @@ public class BusinessService {
   private PagingAndSortingRepository pagingAndSortingRepository;
 
   public Business createNew(Business business) {
-        business.setActive(true);
-        return businessRepo.save(business);
+    log.debug("Creating new business {}", business);
+    business.setActive(true);
+    return businessRepo.save(business);
 
   }
 
   public List<Business> getAllBusinesses() {
-      return businessRepo.findAll();
+    return businessRepo.findAll();
   }
 
   public void deactivateBusiness(Long businessId) {
-    updateBusinessStatus(businessId,false);
+    log.debug("Deactivating business {}", businessId);
+    updateBusinessStatus(businessId, false);
   }
 
-  private void updateBusinessStatus(Long businessId,boolean status) {
+  private void updateBusinessStatus(Long businessId, boolean status) {
+    log.debug("Updating business {} to status {}", businessId, status);
     Optional<Business> businessOptional = businessRepo.findById(businessId);
-    if(businessOptional.isPresent()) {
+    if (businessOptional.isPresent()) {
       Business business = businessOptional.get();
       business.setActive(status);
       businessRepo.save(business);
--- src/main/java/com/local/kattalocal/kattapp/service/EventsService.java
+++ src/main/java/com/local/kattalocal/kattapp/service/EventsService.java
@@ -7,16 +7,21 @@ import com.local.kattalocal.kattapp.model.projection.NearByOffers;
 import com.local.kattalocal.kattapp.repository.BusinessRepo;
 import com.local.kattalocal.kattapp.repository.EventsRepository;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class EventsService {
 
-  @Autowired private EventsRepository eventsRepository;
-  @Autowired private BusinessRepo businessRepo;
+  @Autowired
+  private EventsRepository eventsRepository;
+  @Autowired
+  private BusinessRepo businessRepo;
 
-  public Events save(long businessId , Events events) {
+  public Events save(long businessId, Events events) {
+    log.debug("Saving business {} with events {}", businessId, events);
     Business business = businessRepo.findById(businessId).get();
     events.setBusiness(business);
     events.setActive(true);
@@ -24,14 +29,17 @@ public class EventsService {
   }
 
   public List<Events> getEventsForBusiness(long businessId) {
+    log.debug("Getting events for business {}", businessId);
     return eventsRepository.findByBusinessId(businessId);
   }
 
-  public List<NearByEvents> getNearByEvents(double lat , double lon) {
-    return eventsRepository.findNearByEvents(lat,lon);
+  public List<NearByEvents> getNearByEvents(double lat, double lon) {
+    log.debug("Getting near by events in lat {} and long {}", lat, lon);
+    return eventsRepository.findNearByEvents(lat, lon);
   }
 
   public Events invalidateEvent(Long eventId) {
+    log.debug("Invalidating event {}", eventId);
     Events event = eventsRepository.findById(eventId).get();
     event.setActive(false);
     return eventsRepository.save(event);
