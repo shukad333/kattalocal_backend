c1d9a2d858574b06166a8e94efd21e01fa75a4ec
Shuhail Kadavath <shukad333@gmail.com>
2023-08-09 10:58:06 +0530

    skeleton analytics controller | validations

--- src/main/java/com/local/kattalocal/kattapp/controller/v1/AnalyticsController.java
+++ src/main/java/com/local/kattalocal/kattapp/controller/v1/AnalyticsController.java
@@ -1,8 +1,23 @@
 package com.local.kattalocal.kattapp.controller.v1;
 
+import com.local.kattalocal.kattapp.model.Clicks;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AnalyticsController {
+
+  @PostMapping("/v1/analytics")
+  public ResponseEntity<Clicks> newAnalytics() {
+    return null;
+  }
+
+  @GetMapping("/v1/analytics")
+  public ResponseEntity<List<Clicks>> getAllAnalytics() {
+    return null;
+  }
 
 }
