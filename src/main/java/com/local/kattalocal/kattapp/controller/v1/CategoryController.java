d5f50eb843cff3e361a65061f2fde37bd8796afb
Shuhail Kadavath <shukad333@gmail.com>
2023-08-19 23:17:04 +0530

    subcategories of cateories API

--- src/main/java/com/local/kattalocal/kattapp/controller/v1/CategoryController.java
+++ src/main/java/com/local/kattalocal/kattapp/controller/v1/CategoryController.java
@@ -3,6 +3,7 @@ package com.local.kattalocal.kattapp.controller.v1;
 import com.local.kattalocal.kattapp.model.Category;
 import com.local.kattalocal.kattapp.service.CategoryService;
 import jakarta.transaction.Transactional;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +20,12 @@ public class CategoryController {
   @GetMapping(value = "/v1/categories")
   public ResponseEntity<Category> getCategoryByName(@RequestParam("name") String name) {
     return ResponseEntity.ok(categoryService.findByName(name));
+  }
+
+  @GetMapping(value = "/v1/categories/subcategories")
+  public ResponseEntity<List<Category>> getSubCategoriesOfCategories(
+      @RequestParam("name") String name) {
+    return null;
   }
 
 }
