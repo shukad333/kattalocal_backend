ec4ad262b80d0b8c58e4e36a9aa40f3f58a4e251
Shuhail Kadavath <shukad333@gmail.com>
2023-08-25 22:38:03 +0530

    Business Model Image Support

--- src/main/java/com/local/kattalocal/kattapp/model/Business.java
+++ src/main/java/com/local/kattalocal/kattapp/model/Business.java
@@ -34,6 +34,25 @@ public class Business implements Serializable {
   private String address2;
   private String address3;
 
+  private String image1;
+  private String image2;
+
+  public String getImage1() {
+    return image1;
+  }
+
+  public void setImage1(String image1) {
+    this.image1 = image1;
+  }
+
+  public String getImage2() {
+    return image2;
+  }
+
+  public void setImage2(String image2) {
+    this.image2 = image2;
+  }
+
   @Enumerated(EnumType.STRING)
   @Column(name = "business_status")
   private BusinessStatus businessStatus;
