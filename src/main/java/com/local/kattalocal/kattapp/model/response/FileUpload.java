dca0d7775aa9efba45aafa0b81a523c4e512d639
Shuhail Kadavath <shukad333@gmail.com>
2023-09-02 16:52:38 +0530

    file upload model

--- src/main/java/com/local/kattalocal/kattapp/model/response/FileUpload.java
+++ src/main/java/com/local/kattalocal/kattapp/model/response/FileUpload.java
@@ -3,5 +3,21 @@ package com.local.kattalocal.kattapp.model.response;
 public class FileUpload {
 
   private String fileName;
+  private String url;
 
+  public String getFileName() {
+    return fileName;
+  }
+
+  public void setFileName(String fileName) {
+    this.fileName = fileName;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
 }
